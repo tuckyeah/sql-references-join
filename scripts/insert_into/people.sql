7a543846e09c29e71bb1f27ed2f6370e3441d5a3
Antony Donovan <antony.donovan@generalassemb.ly>
2016-02-16 16:33:10 -0500

    Improve column names
    dob -> born_on

--- scripts/insert_into/people.sql
+++ scripts/insert_into/people.sql
@@ -1,9 +1,8 @@
 -- create rows with data about people
-INSERT INTO people(surname,given_name,gender,height,weight,dob)
+INSERT INTO people(surname,given_name,gender,height,weight,born_on)
 VALUES ('Rollins', 'Henry', 'man', 69, 180, '1961-02-13');
 
 /*
  VALUES ('Rollins', 'Henry', 'm', 69, 180, '1961-02-13'),
        ('T', 'Mister', 'm', 70, 200, '1952-05-21');
 */
-
